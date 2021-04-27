c52914816657c91a033a68775ea2b92c8b784c9a
Tadeo Villafaña <tadeoova@gmail.com>
2021-04-27 20:30:58 -0300

    primer commit

---
+++ src/Game.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PengineClient from './PengineClient';
+import Board from './Board';
+
+class Game extends React.Component {
+
+  pengine;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: null,
+      waiting: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handlePengineCreate = this.handlePengineCreate.bind(this);
+    this.pengine = new PengineClient(this.handlePengineCreate);
+  }
+
+  handlePengineCreate() {
+    const queryS = 'init(PistasFilas, PistasColumns, Grilla)';
+    this.pengine.query(queryS, (success, response) => {
+      if (success) {
+        this.setState({
+          grid: response['Grilla']
+        });
+      }
+    });
+  }
+
+  handleClick(i, j) {
+    // No action on click if we are waiting.
+    if (this.state.waiting) {
+      return;
+    }
+    // Build Prolog query to make the move, which will look as follows:
+    // put("#",[0,1],[], [],[["X",_,_,_,_],["X",_,"X",_,_],["X",_,_,_,_],["#","#","#",_,_],[_,_,"#","#","#"]], GrillaRes, FilaSat, ColSat)
+    const squaresS = JSON.stringify(this.state.grid).replaceAll('"_"', "_"); // Remove quotes for variables.
+    const queryS = 'put("#", [' + i + ',' + j + ']' 
+    + ', [], [],' + squaresS + ', GrillaRes, FilaSat, ColSat)';
+    this.setState({
+      waiting: true
+    });
+    this.pengine.query(queryS, (success, response) => {
+      if (success) {
+        this.setState({
+          grid: response['GrillaRes'],
+          waiting: false
+        });
+      } else {
+        this.setState({
+          waiting: false
+        });
+      }
+    });
+  }
+
+  render() {
+    if (this.state.grid === null) {
+      return null;
+    }
+    const statusText = 'Keep playing!';
+    return (
+      <div className="game">
+        <Board
+          grid={this.state.grid}
+          onClick={(i, j) => this.handleClick(i,j)}
+        />
+        <div className="gameInfo">
+          {statusText}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
