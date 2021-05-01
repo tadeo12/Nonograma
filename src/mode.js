451b0584f345d26f4faec9297fff293780ed3284
QuimeyRodi <quimeyrodi@gmail.com>
2021-05-01 17:38:12 -0300

    class mode

---
+++ src/mode.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class mode extends React.Component{
+    render(){
+        return(
+            <button className= "mode" onClick={this.props.onClick}>
+                {}
+            </button>
+        );
+    }
+
+}
+
+export default mode;
