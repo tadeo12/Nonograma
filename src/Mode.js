aafd8487ca914eba6588a627b0f38f293d8ef431
Tadeo Villafaña <tadeoova@gmail.com>
2021-05-13 20:11:56 -0300

    animation done! :)

--- src/Mode.js
+++ src/Mode.js
@@ -5,10 +5,10 @@ class Mode extends Component{
     render(){
         return(
             <button id = "modeId" className= {this.props.modo === "#" ? "modeButton paint" : "modeButton" } onClick={this.props.onClick}>
-                <span className =  "span top-line"></span>
-                <span className = "span right-line"></span>
-                <span className = "span left-line"></span>
-                <span className = "span bottom-line"></span>                
+                <span className =  {this.props.modo === "#" ? "span paint-top-line" : "span top-line" }></span>
+                <span className = {this.props.modo === "#" ? "span paint-right-line" : "span right-line" }></span>
+                <span className = {this.props.modo === "#" ? "span paint-left-line" : "span left-line" }></span>
+                <span className = {this.props.modo === "#" ? "span paint-bottom-line" : "span bottom-line" }></span>                
             </button>          
           
         );
