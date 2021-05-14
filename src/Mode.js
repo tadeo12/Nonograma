d8143ed934c4cfdd0456fae5fae961808a62bc8d
Tadeo Villafaña <tadeoova@gmail.com>
2021-05-14 13:01:08 -0300

    desabilitar modo gano

--- src/Mode.js
+++ src/Mode.js
@@ -5,7 +5,7 @@ class Mode extends Component{
     render(){
         
         return(
-            <button  disabled = {this.props.gano ? "disabled": "enabled"} id = "modeId" className= {this.props.modo === "#" ? "button paint" : "button" } onClick={this.props.onClick}>
+            <button  disabled = {this.props.gano ? true: false} id = "modeId" className= {this.props.modo === "#" ? "button paint" : "button" } onClick={this.props.onClick}>
                 <span className =  {this.props.modo === "#" ? "span paint-top-line" : "span top-line" }></span>
                 <span className = {this.props.modo === "#" ? "span paint-right-line" : "span right-line" }></span>
                 <span className = {this.props.modo === "#" ? "span paint-left-line" : "span left-line" }></span>
